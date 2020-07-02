85421e965b83401cd2c8ba266da00f77233e34ec
spencerng94 <spencerng94@gmail.com>
2020-07-01 22:50:50 -0700

    Refactored server proxy routes for all components

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const axios = require('axios');
+const path = require('path')
 const { createProxyMiddleware } = require('http-proxy-middleware');
 var cors = require('cors');
-var proxy = require('express-http-proxy');
 
 const app = express();
 
@@ -12,10 +12,17 @@ const port = 4000;
 
 app.use(cors());
 
-app.use('/proxy', proxy('www.google.com'));
+app.use('/products', createProxyMiddleware({ target: `http://localhost:3000`, changeOrigin: true }));
+app.use('/', createProxyMiddleware({ target: `http://localhost:3002`, changeOrigin: true }));
+app.use('/', createProxyMiddleware({ target: `http://localhost:3003`, changeOrigin: true }));
+app.use('/reviews', createProxyMiddleware({ target: `http://localhost:3004`, changeOrigin: true }));
 
-app.use('/api/main-product', createProxyMiddleware({ target: `http://localhost:3000`, changeOrigin: true }));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/../public/index.html'));
+})
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
+
+
